268c390d678a0e17877665d157c5b11dfb75774f
Damien Buttler <damien@doublehops.com>
2012-04-19 22:38:17 +1000

    getting there.

--- runQuery.php
+++ runQuery.php
@@ -8,10 +8,12 @@
         private $session;
         private $view;
         private $dbConn;
+        private $alertMessages = array();
 
         public function __construct()
         {
             $this->session = new Session();
+            $this->dbConn = new DB();
             $this->view = new View();
         }
 
@@ -19,16 +21,27 @@
         {
             $action = isset($_POST['action']) ? $_POST['action'] : false;
 
-            if($action == 'login')
+            if($this->session->isActiveSessionGood())
             {
-                $this->dbConn = new DB();
-                if($this->dbConn->connect
-                
+                $this->dbConn->connect($_SESSION['mysqlHost'], $_SESSION['mysqlDatabase'], $_SESSION['mysqlUsername'], $_SESSION['mysqlPassword']);    
             }
-
-            if($this->session->isActiveSession())
+            elseif($action == 'login')
             {
-                
+                $this->session->saveInputData($_POST);
+
+                if($this->dbConn->connect($_POST['mysqlHost'], $_POST['mysqlDatabase'], $_POST['mysqlUsername'], $_POST['mysqlPassword']))
+                {
+                    $this->session->savePassword($_POST['mysqlPassword']);
+                    $this->view->render('queryBox');
+                }
+                else
+                {
+                    $this->view->render('loginForm', $this->dbConn->getErrors());
+                }
+            }
+            elseif($action == 'runQuery')
+            {
+            
             }
             else
             {
@@ -48,16 +61,23 @@
             try
             {
                 $dsn = 'mysql:host='. $host .';dbname='. $database;
-                $dbh = new PDO($dsn, $username, $password);
-                $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
-                $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->dbh = new PDO($dsn, $username, $password);
+                $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+                $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $this->dbh->query( 'show tables' );  // Basic query to force a test of the connection
             }
-            catch
+            catch (PDOException $e)
             {
-                $this->error = $e->getMessage();
+                $this->errorMsg[] = $e->getMessage();
             }
 
             return $this->isErrorFree() ? true : false;
+        }
+
+        public function getErrors()
+        {
+            return $this->errorMsg;
         }
 
         public function isErrorFree()
@@ -78,21 +98,51 @@
             session_start();
         }
 
-        public function isActiveSession()
+        public function isActiveSessionGood()
         {
 
             return (isset($_SESSION['dbConnGood']) && $_SESSION['dbConnGood'] == true) ? true : false;
+        }
+
+        public function setActiveSessionGood()
+        {
+            $_SESSION['dbConnGood'] = true;
+        }
+
+        public function saveInputData($post)
+        {
+            $_SESSION['mysqlHost'] = $post['mysqlHost'];
+            $_SESSION['mysqlDatabase'] = $post['mysqlDatabase'];
+            $_SESSION['mysqlUsername'] = $post['mysqlUsername'];
+        }
+
+        public function savePassword($password)
+        {
+            $_SESSION['mysqlPassword'] = $password;
         }
 
     }
 
     class View 
     {
-        public function render($view)
+        public function render($view, $messages=false)
         {
             $this->header();
+            if($messages != false) $this->printMessages($messages);
             $this->$view();
             $this->footer();
+        }
+
+        private function printMessages($messages)
+        {
+            ?>
+
+                <div id="messages">
+                    <?php foreach($messages as $message) : ?>
+                        <p><?php echo $message ?></p>
+                    <?php endforeach; ?>
+                </div> <!-- end messages -->
+            <?php
         }
 
         private function queryBox()
@@ -112,18 +162,22 @@
 
         private function loginForm()
         {
+            $mysqlHost = isset($_SESSION['mysqlHost']) ? $_SESSION['mysqlHost'] : 'localhost';
+            $mysqlDatabase = isset($_SESSION['mysqlDatabase']) ? $_SESSION['mysqlDatabase'] : '';
+            $mysqlUsername = isset($_SESSION['mysqlUsername']) ? $_SESSION['mysqlUsername'] : '';
+
             ?>
                 <form action="" method="post">
                     <div id="loginForm">
                         <dl>
                             <dt><label for="mysqlHost">MySQL Host</label></dt>
-                            <dd><input type="text" name="mysqlHost" id="mysqlHost" value="" /></dd>
+                            <dd><input type="text" name="mysqlHost" id="mysqlHost" value="<?php echo $mysqlHost ?>" /></dd>
                             <dt><label for="mysqlDatabase">MySQL Database</label></dt>
-                            <dd><input type="text" name="mysqlDatabase" id="mysqlDatabase"value="" /></dd>
+                            <dd><input type="text" name="mysqlDatabase" id="mysqlDatabase" value="<?php echo $mysqlDatabase?>" /></dd>
                             <dt><label for="mysqlUsername">MySQL Username</label></dt>
-                            <dd><input type="text" name="mysqlUsername" id="mysqlUsername" value="" /></dd>
+                            <dd><input type="text" name="mysqlUsername" id="mysqlUsername" value="<?php echo $mysqlUsername ?>" /></dd>
                             <dt><label for="mysqlPassword">MySQL Password</label></dt>
-                            <dd><input type="text" name="mysqlPassword" id="mysqlPassword" value="" /></dd>
+                            <dd><input type="password" name="mysqlPassword" id="mysqlPassword" /></dd>
                             <input type="hidden" name="action" value="login" />
                             <input type="submit" name="submit" value="submit" />
                         </dl>
